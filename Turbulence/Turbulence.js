b4f6d774ca5c296b25a4b4213025c3a334a912c6
kecso <tkecskes@isis.vanderbilt.edu>
2014-04-25 13:18:59 -0500

    bugfix in error branch

--- Turbulence/Turbulence.js
+++ Turbulence/Turbulence.js
@@ -145,7 +145,8 @@ define(['plugin/PluginConfig','plugin/PluginBase','util/assert'],function(Plugin
 
     self.logger.warn('Script generation');
     if (proc_definitions === null) {
-      return;
+        self.result.setSuccess(false);
+        return callback(null);
     }
 
     var pre_script = '//    Copyright 2011 Johns Hopkins University\n//\n//  Licensed under the Apache License, Version 2.0 (the "License");\n//  you may not use this file except in compliance with the License.\n//  You may obtain a copy of the License at\n//\n//      http://www.apache.org/licenses/LICENSE-2.0\n//\n//  Unless required by applicable law or agreed to in writing, software\n//  distributed under the License is distributed on an "AS IS" BASIS,\n//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n//  See the License for the specific language governing permissions and\n//  limitations under the License.\n\n\n#include <stdio.h>\n#include <float.h>\n#include <math.h>\n#include "turblib.h"\n#include "spline_interp_lib.h"\n\nint main(int argc, char *argv[])\n{\n';
